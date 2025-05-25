4a4ef95185a4233c65dd9b5aae8ffb3625593956
festusrono <festusrono56@gmail.com>
2025-05-25 12:49:22 +0200

    autoplay navs

--- js/main.js
+++ js/main.js
@@ -1,17 +1,61 @@
 //Swiper
-var swiper = new swiper('.swiper-container', {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+// var swiper = new swiper('.swiper-container', {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+// });
+
+
+
+
+ //<!-- Initialize Swiper -->
+
+      var swiper = new Swiper(".popular-content", {
+        slidesPerView:1,
+        spaceBetween: 10,
+        autoplay: {
+          delay: 755500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+          280: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+            758: {
+                slidesPerView: 3,
+                spaceBetween: 15,
+            },
+            900: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+        },
+      });
